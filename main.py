e47b5ce758b87f24d8c9f3b7e353b326970cbd17
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-16 06:12:58 +0200

    Copying and moving cells

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ ws = wb.active
 # change the value
 # ws['A2'].value = 'Sam'
 
-# Saving
-wb.save('Grades.xlsx')
 
 # Creating a new sheet
 # wb.create_sheet('new sheet')
@@ -15,7 +13,20 @@ wb.save('Grades.xlsx')
 # print(wb.sheetnames)
 
 # Merging cells
-ws.merge_cells('A1:D1')
+# ws.merge_cells('A1:D1')
+
+# Unmerging cells
+# ws.unmerge_cells('A1:D1')
+
+# Insert an empty rows (after!)
+# ws.insert_rows(7)
+
+
+# Delete (after!)
+# ws.delete_rows(7)
+
+# Copying and moving cells
+# ws.move_range('C1:D11', rows=2, cols=2)
 
 
 for row in range(1, 11):
@@ -23,3 +34,7 @@ for row in range(1, 11):
         char = get_column_letter(col)
         # print(ws[char + str(row)].value)
         ws[char + str(row)] = char + str(row)
+
+
+# Saving
+wb.save('Grades.xlsx')
